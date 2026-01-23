2f2c3b6987196a11edcf3edeef9e4cdf60a3e1fc
[sneha suman] <snehasuman1119@gmail.com>
2026-01-23 23:18:46 +0530

    String Basics

---
+++ 01.Basics/StringBasics.java
@@ -0,0 +1,12 @@
+public class StringBasics {
+    public static void main(String[] args) {
+
+        String name = "Sneha";
+        String language = "Java";
+
+        System.out.println(name);
+        System.out.println(language);
+        System.out.println(name.length());
+        System.out.println(name + " is learning " + language);
+    }
+}
